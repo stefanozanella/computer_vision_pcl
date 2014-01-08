68108541578ffdb8fa28528f9d67eadb0157faa1
Stefano Zanella <zanella.stefano@gmail.com>
2014-01-08 12:35:21 +0100

    Refine ex #3.

--- src/ex3.cpp
+++ src/ex3.cpp
@@ -1,4 +1,15 @@
+/**
+ * PCL Lab - Ex #3
+ *
+ * Compute normals with different search radiuses on the same point cloud and
+ * display the results side-by-side.
+ *
+ * Author: Stefano Zanella
+ * Date: 08/01/2014
+ */
+
 #include <iostream>
+#include <vector>
 
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
@@ -9,6 +20,7 @@
 
 using std::cout;
 using std::endl;
+using std::vector;
 
 using pcl::PointCloud;
 using pcl::PointXYZRGB;
@@ -18,6 +30,7 @@ using pcl::io::loadPCDFile;
 using pcl::search::KdTree;
 using pcl::visualization::PCLVisualizer;
 using pcl::visualization::PointCloudColorHandlerRGBField;
+using pcl::removeNaNFromPointCloud;
 
 int main(int argc, char** argv) {
   PointCloud<PointXYZRGB>::Ptr cloud_in (new PointCloud<PointXYZRGB>);
@@ -30,6 +43,9 @@ int main(int argc, char** argv) {
   }
 
   cout << "Loaded point cloud: " << cloud_in->width << " x " << cloud_in->height << endl;
+
+  vector<int> indices;
+  removeNaNFromPointCloud(*cloud_in, *cloud_in, indices);
 
 	NormalEstimationOMP<PointXYZRGB, Normal> ne;
 	ne.setSearchMethod(KdTree<PointXYZRGB>::Ptr(new KdTree<PointXYZRGB>));
@@ -51,35 +67,43 @@ int main(int argc, char** argv) {
 
 	PCLVisualizer viewer("PCL Viewer");
 	viewer.initCameraParameters();
-	PointCloudColorHandlerRGBField<PointXYZRGB> rgb (cloud_in);
 
-	int left_viewport (0);
+	int left_viewport, right_viewport;
 	viewer.createViewPort(0.0, 0.0, 0.5, 1.0, left_viewport);
-	viewer.setBackgroundColor(0, 0, 0.5, left_viewport);
-	viewer.addCoordinateSystem(0.1, left_viewport);
-	viewer.addText("Coarse grain normals", 10, 10, "left_viewport text", left_viewport);
+	viewer.createViewPort(0.5, 0.0, 1.0, 1.0, right_viewport);
+	viewer.setBackgroundColor(0, 0, 0.5);
+	viewer.addCoordinateSystem(0.1);
+
+	viewer.addText("Coarse grain normals", 10, 10, "left_viewport_text", left_viewport);
+	viewer.addText("Fine grain normals", 10, 10, "right_viewport_text", right_viewport);
+
 	viewer.addPointCloud<PointXYZRGB>(
       cloud_in,
       PointCloudColorHandlerRGBField<PointXYZRGB>(cloud_in),
       "input_cloud_left",
       left_viewport);
-	viewer.addPointCloudNormals<PointXYZRGB, Normal>(cloud_in, cloud_coarse_normals, normalsVisualizationStep, normalsScale, "normals_left", left_viewport);
+	viewer.addPointCloudNormals<PointXYZRGB, Normal>(cloud_in,
+      cloud_coarse_normals,
+      normalsVisualizationStep,
+      normalsScale,
+      "normals_left",
+      left_viewport);
 
-	int right_viewport (0);
-	viewer.createViewPort(0.5, 0.0, 1.0, 1.0, right_viewport);
-	viewer.setBackgroundColor(0, 0, 0.5, right_viewport);
-	viewer.addCoordinateSystem(0.1, right_viewport);
-	viewer.addText("Fine grain normals", 10, 10, "right_viewport text", right_viewport);
 	viewer.addPointCloud<PointXYZRGB>(
       cloud_in,
       PointCloudColorHandlerRGBField<PointXYZRGB>(cloud_in),
       "input_cloud_right",
       right_viewport);
-	viewer.addPointCloudNormals<PointXYZRGB, Normal>(cloud_in, cloud_fine_normals, normalsVisualizationStep, normalsScale, "normals_right", right_viewport);
+	viewer.addPointCloudNormals<PointXYZRGB, Normal>(cloud_in,
+      cloud_fine_normals,
+      normalsVisualizationStep,
+      normalsScale,
+      "normals_right",
+      right_viewport);
 
 	cout << "Visualizing..."<< endl;
 
   viewer.spin();
+
   return 0;
 }
-
